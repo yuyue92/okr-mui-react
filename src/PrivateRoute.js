b86bebff675d738929d044f33b46a13271b4d7d8
yuyue92 <yy18825237023@126.com>
2026-07-09 11:18:11 +0800

    Delete src directory

--- src/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import useStore from './store';
-
-export default function PrivateRoute({ children }) {
-  const { user } = useStore();
-  return user ? children : <Navigate to="/login" replace />;
-}
